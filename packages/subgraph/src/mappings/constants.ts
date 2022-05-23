757b2dac52d24af7ce086c4d31d6943e760bbb80
Santiago Algozino <salgozino@gmail.com>
2022-05-22 21:19:03 -0300

    subgraph: fix realitio address for kovan

--- packages/subgraph/src/mappings/constants.ts
+++ packages/subgraph/src/mappings/constants.ts
@@ -1,5 +1,5 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
 
-export const RealitioAddress = Address.fromHexString("0xE78996A233895bE74a66F451f1019cA9734205cc");
+export const RealitioAddress = Address.fromHexString("0xcB71745d032E16ec838430731282ff6c10D29Dea");
 
 export const correctAnswerPoints = BigInt.fromI32(1);
