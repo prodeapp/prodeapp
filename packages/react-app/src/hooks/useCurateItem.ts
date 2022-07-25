152da9b174e4af4885f28ed6b15140a93f8d642b
Santiago Algozino <salgozino@gmail.com>
2022-07-25 02:42:56 -0300

    react: fix hook useCurateItems

--- packages/react-app/src/hooks/useCurateItem.ts
+++ packages/react-app/src/hooks/useCurateItem.ts
@@ -15,12 +15,10 @@ export const useCurateItem = (marketHash: string) => {
   return useQuery<CurateItem, Error>(
     ["useCurateItems", marketHash],
     async () => {
-
-      const response = await apolloProdeQuery<{ curateItem: CurateItem }>(query, {hash: marketHash});
+      const response = await apolloProdeQuery<{ curateItems: CurateItem[] }>(query, {hash: marketHash});
 
       if (!response) throw new Error("No response from TheGraph");
-      // console.log(response.data)
-      return response.data.curateItem;
+      return response.data.curateItems[0];
     }
   );
 };
