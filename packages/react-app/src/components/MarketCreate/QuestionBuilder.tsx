10ffdb974aea0369cfab5cce52467cf28cf5fc31
Rodrigo Souto <rodrigo_7@live.com.ar>
2022-09-01 17:45:04 -0300

    event name: require it to be a question

--- packages/react-app/src/components/MarketCreate/QuestionBuilder.tsx
+++ packages/react-app/src/components/MarketCreate/QuestionBuilder.tsx
@@ -16,7 +16,8 @@ export default function QuestionBuilder({eventIndex}: QuestionBuilderProps) {
   return <div style={{flexGrow: 1}}>
     <div style={{display: 'flex'}}>
       <TextField {...register(`events.${eventIndex}.questionPlaceholder`, {
-        required: t`This field is required.`
+        required: t`This field is required.`,
+        validate: v => v.includes('?') || t`The event name must be a question written in english, for example: 'Who will win the match between Barcelona and Real Madrid at the Champions League 2022/2023 Final?'`,
       })} error={!!errors.events?.[eventIndex]?.questionPlaceholder} style={{flexGrow: 1}}/>
     </div>
     <FormError><ErrorMessage errors={errors} name={`events.${eventIndex}.questionPlaceholder`} /></FormError>
