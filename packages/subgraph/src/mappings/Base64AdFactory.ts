45a17abe3ce745b204d65477e9673120dea879fd
Algozino Santiago <salgozino@gmail.com>
2022-09-18 22:52:38 -0300

    subgraph: add svg in the ad entity

--- packages/subgraph/src/mappings/Base64AdFactory.ts
+++ packages/subgraph/src/mappings/Base64AdFactory.ts
@@ -1,9 +1,16 @@
 import { Base64Ad } from '../types/schema';
-import { NewAd } from '../types/Base64AdFactory/Base64AdFactory';
+import { CreateAdCall, NewAd } from '../types/Base64AdFactory/Base64AdFactory';
 
 
 export function handleNewAd(event: NewAd): void {
     let base64Ad = new Base64Ad(event.params.ad.toHexString());
+    base64Ad.markets = [];
+    base64Ad.save()
+}
+
+export function handleCreateAd(call: CreateAdCall): void {
+    let base64Ad = new Base64Ad(call.outputs.value0.toHexString());
+    base64Ad.svg = call.inputs._svg;
     base64Ad.markets = [];
     base64Ad.save()
 }
