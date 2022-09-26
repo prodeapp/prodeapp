046c81883bcd01eb388b8cfa1b3557bc50574bb9
Algozino Santiago <salgozino@gmail.com>
2022-09-26 19:33:52 -0300

    subgraph ads: mapping for event in curate of NewItem

--- packages/subgraph/src/mappings/TechnicalCurate.ts
+++ packages/subgraph/src/mappings/TechnicalCurate.ts
@@ -1,9 +1,10 @@
 /* eslint-disable prefer-const */
 import { Address, Bytes, log } from '@graphprotocol/graph-ts';
-import { CurateBase64AdItem } from '../types/schema';
+import { CurateAdsMapper, CurateBase64AdItem } from '../types/schema';
 
 import {
   GeneralizedTCR,
+  ItemSubmitted,
   ItemStatusChange,
 } from '../types/ContentCurate/GeneralizedTCR';
 import { getDataFromItemID, getStatusFromItemID } from './GeneralizedTCR';
@@ -17,9 +18,6 @@ export function handleItemStatusChange(evt: ItemStatusChange): void {
   log.debug("handleItemSubmitted: itemID {} for technicalCurate itemID {}", [itemID, evt.params._itemID.toHexString()])
   let curateItem = CurateBase64AdItem.load(itemID);
 
-  let data = getDataFromItemID(evt.params._itemID, evt.address);
-  log.debug("handleItemStatusChange: data = {}", [data.toHexString()]);
-
   if (curateItem === null) {
     log.error('handleItemStatusChange: CurateBase64AdItem with itemId {} not found for technicalItemID {}', [
       itemID,
@@ -27,7 +25,18 @@ export function handleItemStatusChange(evt: ItemStatusChange): void {
     ]);
     return;
   }
+
+  let data = getDataFromItemID(evt.params._itemID, evt.address);
+  log.debug("handleItemStatusChange: data = {}", [data.toHexString()]);
   curateItem.technicalStatus = getStatusFromItemID(curateItem.technicalItemID, evt.address)
   log.debug("handleItemStatusChange: ItemID {} has status {} in technicalCurate", [evt.params._itemID.toHexString(), curateItem.technicalStatus])
   curateItem.save();
+}
+
+export function handleItemSubmitted(evt: ItemSubmitted): void {
+  let curateMapper = new CurateAdsMapper(evt.params._itemID.toHexString())
+  const data = getDataFromItemID(evt.params._itemID, evt.address);
+  log.debug("handleItemSubmitted: Data {}", [data.toHexString()]);
+  curateMapper.curateBase64AdItem = data.slice(3, 69).toString();
+  curateMapper.save()
 }
