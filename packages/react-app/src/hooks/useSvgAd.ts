7daaa4b9f3834143355f030d319f7c3b9870d91a
Rodrigo Souto <rodrigo_7@live.com.ar>
2022-10-08 17:22:08 -0300

    refactor

--- packages/react-app/src/hooks/useSvgAd.ts
+++ packages/react-app/src/hooks/useSvgAd.ts
@@ -1,19 +1,13 @@
 import {useCall} from "@usedapp/core";
 import {Contract} from "@ethersproject/contracts";
-import {useEffect, useState} from "react";
 import {SVG__factory} from "../typechain";
 
 export const useSvgAd = (SVGAd: string) => {
-  // TODO: fix args
   const { value: tokenUri } = useCall({ contract: new Contract(SVGAd, SVG__factory.createInterface()), method: 'getSVG', args: [SVGAd, 0] }) || {}
 
-  const [image, setImage] = useState('');
+  if (tokenUri !== undefined) {
+    return tokenUri[0];
+  }
 
-  useEffect(() => {
-    if (tokenUri !== undefined) {
-      setImage(tokenUri[0]);
-    }
-  }, [tokenUri]);
-
-  return image;
+  return '';
 };
