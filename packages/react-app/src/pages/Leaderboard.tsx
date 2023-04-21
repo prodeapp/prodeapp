06bfc5b56ee95b3a6ea2a97d1f6e387f8d53b290
rodsouto <rodrigo_7@live.com.ar>
2023-04-21 01:51:04 -0300

    remove buttons + refactor

--- packages/react-app/src/pages/Leaderboard.tsx
+++ packages/react-app/src/pages/Leaderboard.tsx
@@ -1,5 +1,4 @@
 import { Trans } from '@lingui/macro'
-import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Skeleton from '@mui/material/Skeleton'
@@ -9,7 +8,6 @@ import { BigNumber, BigNumberish } from 'ethers'
 import { useState } from 'react'
 import { useNetwork } from 'wagmi'
 
-import { BoxRow, BoxWrapper } from '@/components'
 import { useLeaderboard } from '@/hooks/useLeaderboard'
 import { useMarketFactory } from '@/hooks/useMarketFactory'
 import { filterChainId } from '@/lib/config'
@@ -39,10 +37,6 @@ export default function Leaderboard() {
 	const chainId = filterChainId(chain?.id)
 	const { isLoading, data: leaderboard } = useLeaderboard()
 	const { data: marketFactory } = useMarketFactory()
-	const [sorting, setSorting] = useState<'numOfBets' | 'numOfMarkets' | 'pricesReceived' | 'amountBet' | 'pnl' | 'roi'>(
-		'pricesReceived'
-	)
-	const [direction, setDirection] = useState<'asc' | 'desc' | null>('desc')
 	const [pageSize, setPageSize] = useState<number>(10)
 	const [sortModel, setSortModel] = useState<GridSortModel>([
 		{
@@ -109,129 +103,61 @@ export default function Leaderboard() {
 	return (
 		<Container style={{ width: '100%', marginTop: '20px' }}>
 			<Typography variant='h5'>
-				<Trans>Global Metrics:</Trans>
+				<Trans>Global Metrics</Trans>
 			</Typography>
-			<Grid container columnSpacing={2} rowSpacing={1} sx={{ marginTop: '30px', justifyContent: 'space-between' }}>
-				<Grid item sm={12} md={3} sx={{ alignItems: 'center', justifyContent: 'center' }}>
-					<BoxWrapper sx={{ padding: 2 }}>
-						<Typography variant='h6'>
-							<Trans>Total Bets</Trans>:{' '}
-							{marketFactory ? formatAmount(marketFactory.totalVolumeBets, chainId) : <Skeleton />}
-						</Typography>
-					</BoxWrapper>
+			<Grid container columnSpacing={2} rowSpacing={1} sx={{ marginY: '30px', textAlign: 'center' }}>
+				<Grid item sm={12} md={3}>
+					<Typography variant='h6'>
+						<Trans>Total Bets</Trans>:{' '}
+						{marketFactory ? formatAmount(marketFactory.totalVolumeBets, chainId) : <Skeleton />}
+					</Typography>
 				</Grid>
-				<Grid item sm={12} md={3} sx={{ alignItems: 'center', justifyContent: 'center' }}>
-					<BoxWrapper sx={{ padding: 2 }}>
-						<Typography variant='h6'>
-							<Trans>Total Bets</Trans> (#): {marketFactory ? marketFactory.numOfBets : <Skeleton />}
-						</Typography>
-					</BoxWrapper>
+				<Grid item sm={12} md={3}>
+					<Typography variant='h6'>
+						<Trans>Total Bets</Trans> (#): {marketFactory ? marketFactory.numOfBets : <Skeleton />}
+					</Typography>
 				</Grid>
-				<Grid item sm={12} md={3} sx={{ alignItems: 'center', justifyContent: 'center' }}>
-					<BoxWrapper sx={{ padding: 2 }}>
-						<Typography variant='h6'>
-							<Trans
-								id='Total Players: {0}'
-								values={{
-									0: marketFactory ? marketFactory.numOfPlayers : <Skeleton />,
-								}}
-							/>
-						</Typography>
-					</BoxWrapper>
+				<Grid item sm={12} md={3}>
+					<Typography variant='h6'>
+						<Trans
+							id='Total Players: {0}'
+							values={{
+								0: marketFactory ? marketFactory.numOfPlayers : <Skeleton />,
+							}}
+						/>
+					</Typography>
 				</Grid>
-				<Grid item sm={12} md={3} sx={{ alignItems: 'center', justifyContent: 'center' }}>
-					<BoxWrapper sx={{ padding: 2 }}>
-						<Typography variant='h6'>
-							<Trans
-								id='Total Markets: {0}'
-								values={{
-									0: marketFactory ? marketFactory.numOfMarkets : <Skeleton />,
-								}}
-							/>
-						</Typography>
-					</BoxWrapper>
+				<Grid item sm={12} md={3}>
+					<Typography variant='h6'>
+						<Trans
+							id='Total Markets: {0}'
+							values={{
+								0: marketFactory ? marketFactory.numOfMarkets : <Skeleton />,
+							}}
+						/>
+					</Typography>
 				</Grid>
 			</Grid>
-			<Typography variant='h5'>Leaderboard</Typography>
-			<BoxWrapper style={{ marginTop: '20px' }}>
-				<BoxRow>
-					<Grid container>
-						<Grid item sm={8} sx={{ display: 'flex', justifyContent: 'left' }}>
-							<div>
-								<Button
-									onClick={() => setSorting('numOfBets')}
-									color={sorting === 'numOfBets' ? 'secondary' : 'primary'}
-								>
-									# of Bets
-								</Button>
-							</div>
-							<div>
-								<Button
-									onClick={() => setSorting('numOfMarkets')}
-									color={sorting === 'numOfMarkets' ? 'secondary' : 'primary'}
-								>
-									# of Markets
-								</Button>
-							</div>
-							<div>
-								<Button
-									onClick={() => setSorting('pricesReceived')}
-									color={sorting === 'pricesReceived' ? 'secondary' : 'primary'}
-								>
-									Prices Received
-								</Button>
-							</div>
-							<div>
-								<Button
-									onClick={() => setSorting('amountBet')}
-									color={sorting === 'amountBet' ? 'secondary' : 'primary'}
-								>
-									Amount Beted
-								</Button>
-							</div>
-							<div>
-								<Button onClick={() => setSorting('pnl')} color={sorting === 'pnl' ? 'secondary' : 'primary'}>
-									PnL
-								</Button>
-							</div>
-							<div>
-								<Button onClick={() => setSorting('roi')} color={sorting === 'roi' ? 'secondary' : 'primary'}>
-									ROI
-								</Button>
-							</div>
-						</Grid>
-						<Grid item sm={4} sx={{ display: 'flex', justifyContent: 'right' }}>
-							<div>
-								<Button onClick={() => setDirection('asc')} color={direction === 'asc' ? 'secondary' : 'primary'}>
-									Ascending
-								</Button>
-							</div>
-							<div>
-								<Button onClick={() => setDirection('desc')} color={direction === 'desc' ? 'secondary' : 'primary'}>
-									Descending
-								</Button>
-							</div>
-						</Grid>
-					</Grid>
-				</BoxRow>
-			</BoxWrapper>
-			{
-				<DataGrid
-					rows={leaderboard ? leaderboard! : []}
-					columns={columns}
-					loading={isLoading}
-					pageSize={pageSize}
-					onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-					rowsPerPageOptions={[10, 50, 100]}
-					pagination
-					disableSelectionOnClick
-					disableColumnFilter
-					onSortModelChange={(model) => setSortModel(model)}
-					sortModel={sortModel}
-					sortingOrder={['desc', 'asc']}
-					autoHeight={true}
-				/>
-			}
+
+			<Typography variant='h5' sx={{ marginBottom: '30px' }}>
+				Leaderboard
+			</Typography>
+
+			<DataGrid
+				rows={leaderboard ? leaderboard! : []}
+				columns={columns}
+				loading={isLoading}
+				pageSize={pageSize}
+				onPageSizeChange={newPageSize => setPageSize(newPageSize)}
+				rowsPerPageOptions={[10, 50, 100]}
+				pagination
+				disableSelectionOnClick
+				disableColumnFilter
+				onSortModelChange={model => setSortModel(model)}
+				sortModel={sortModel}
+				sortingOrder={['desc', 'asc']}
+				autoHeight={true}
+			/>
 		</Container>
 	)
 }
