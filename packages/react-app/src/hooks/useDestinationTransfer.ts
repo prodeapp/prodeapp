db812a1c86f36a8205b0ccb086a8398c31146f53
Algozino Santiago <salgozino@gmail.com>
2023-05-28 20:59:19 -0300

    revert us of refetchInterval

--- packages/react-app/src/hooks/useDestinationTransfer.ts
+++ packages/react-app/src/hooks/useDestinationTransfer.ts
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query'
-import { useState } from 'react'
 
 import { DestinationTransfer, DESTINATIONTRANSFER_FIELDS } from '@/graphql/subgraph'
 import { apolloConnextQuery } from '@/lib/apolloClient'
@@ -14,28 +13,20 @@ const query = `
 `
 
 export const useDestinationTransfer = (transferId: string | undefined, chainId: number | null) => {
-	const [destinationTransfer, setDestinationTransfer] = useState<null | DestinationTransfer>(null)
-
 	return useQuery<DestinationTransfer, Error>(
 		['useDestinationTransfer', transferId, chainId],
 		async () => {
-			if (
-				destinationTransfer === null ||
-				['Executed', 'CompletedFast', 'CompletedSlow'].indexOf(destinationTransfer?.status) === -1
-			) {
-				const response = await apolloConnextQuery<{ destinationTransfer: DestinationTransfer }>(chainId!, query, {
-					transferId: transferId!.toLowerCase(),
-				})
+			console.log('destination ', transferId)
+			const response = await apolloConnextQuery<{ destinationTransfer: DestinationTransfer }>(chainId!, query, {
+				transferId: transferId!.toLowerCase(),
+			})
 
-				if (!response) throw new Error('No response from TheGraph')
-				setDestinationTransfer(response.data.destinationTransfer)
-				return response.data.destinationTransfer
-			}
-			return destinationTransfer
+			if (!response) throw new Error('No response from TheGraph')
+
+			return response.data.destinationTransfer
 		},
 		{
 			enabled: !!transferId && !!chainId,
-			refetchInterval: 5000,
 		}
 	)
 }
--- packages/react-app/src/hooks/useOriginTransfer.ts
+++ packages/react-app/src/hooks/useOriginTransfer.ts
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query'
-import { useState } from 'react'
 
 import { OriginTransfer, ORIGINTRANSFER_FIELDS } from '@/graphql/subgraph'
 import { apolloConnextQuery } from '@/lib/apolloClient'
@@ -8,29 +7,24 @@ const query = `
     ${ORIGINTRANSFER_FIELDS}
     query OriginTransfer($transferId: String) {
 		originTransfer(id:$transferId) {
-		  chainId
-		  status
+		  ...OriginTransferFields
 		}
 	  }
 `
 
 export const useOriginTransfer = (transferId: string | undefined, chainId: number | undefined) => {
-	const [originTransfer, setOriginTransfer] = useState<OriginTransfer | null>(null)
-
 	return useQuery<OriginTransfer, Error>(
 		['useOriginTransfer', transferId, chainId],
 		async () => {
-			if (originTransfer === null || originTransfer?.status !== 'XCalled') {
-				const response = await apolloConnextQuery<{ originTransfer: OriginTransfer }>(chainId!, query, {
-					transferId: transferId!.toLowerCase(),
-				})
+			console.log('origin ', transferId)
+			const response = await apolloConnextQuery<{ originTransfer: OriginTransfer }>(chainId!, query, {
+				transferId: transferId!.toLowerCase(),
+			})
 
-				if (!response) throw new Error('No response from TheGraph')
-				setOriginTransfer(response.data.originTransfer)
-				return response.data.originTransfer
-			}
-			return originTransfer
+			if (!response) throw new Error('No response from TheGraph')
+
+			return response.data.originTransfer
 		},
-		{ enabled: !!transferId && !!chainId, refetchInterval: 5000 }
+		{ enabled: !!transferId && !!chainId }
 	)
 }
